51bd9674d26f2ed8330578e9f61e56b6cc6c0ff5
jennieines <141196314+jennieines@users.noreply.github.com>
2023-09-21 19:12:01 -0400

    Update wordle2.js
    updates

--- Projects/wordleproject/wordleproject2/wordle2.js
+++ Projects/wordleproject/wordleproject2/wordle2.js
@@ -1,27 +1,85 @@
+// Define an array of words for the game
 let words = ["APPLE", "BEACH", "PIZZA", "GRAPE", "CHAIR", "CHICK", "TABLE", "DRINK", "CROWN"];
-let wordToGuess = Math.floor(Math.random() * words.length);
-let randomWord = words[wordToGuess];
-let word = randomWord;
+
+// Choose a random word from the array
+let wordToGuess = words[Math.floor(Math.random() * words.length)];
+let word = wordToGuess;
+
+// Define the number of allowed guesses
+let maxGuesses = 6;
+let guessesLeft = maxGuesses;
 
 
-function checkWord(guess) {
-if (guess.toLowerCase() === word) {
-  alert("Congratulations! You guessed the word: " + wordToGuess);
-
-} else {
-  console.log("Incorrect guess: " + guess);
-
+// Function to check if the guessed letter is correct
+function checkGuess(letter) {
+  let isCorrect = false;
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === letter) {
+      correctGuesses[i] = true;
+      isCorrect = true;
     }
+  }
+  return isCorrect;
 }
 
-let guessInput = document.getElementById("guess-input");
+// Function to display the current state of the word
+function displayWord() {
+  let displayedWord = "";
+  for (let i = 0; i < word.length; i++) {
+    if (correctGuesses[i]) {
+      displayedWord += word[i];
+    } else {
+      displayedWord += "_";
+    }
+  }
+  return displayedWord;
+}
+
+// Function to handle a guess
+function handleGuess() {
+  let guessInput = document.getElementById("guess-input");
+  let guess = guessInput.value.toUpperCase();
+
+  if (guess.length === 1 && /^[A-Z]$/.test(guess)) {
+    if (checkGuess(guess)) {
+      // Correct guess
+      let displayedWord = displayWord();
+      document.getElementById("word-display").textContent = displayedWord;
+
+      // Check if the word has been completely guessed
+      if (!displayedWord.includes("_")) {
+        // Player won
+        alert("Congratulations! You guessed the word: " + word);
+      }
+    } else {
+      // Incorrect guess
+      guessesLeft--;
+      if (guessesLeft === 0) {
+        // Player lost
+        alert("Sorry, you're out of guesses. The word was: " + word);
+      } else {
+        alert("Incorrect guess. " + guessesLeft + " guesses left.");
+      }
+    }
+  } else {
+    // Invalid input
+    alert("Please enter a single uppercase letter.");
+  }
+
+  // Clear the input field
+  guessInput.value = "";
+}
+
+  // Attach an event listener to the guess button
 let guessButton = document.getElementById("guess-button");
+guessButton.addEventListener("click", handleGuess);
 
-guessButton.addEventListener("click", function() {
-  let guess = guessInput.value.toLowerCase();
-  checkWord(guess);
-});
-// let currentRow = document.getElementById(`row${guess}`)
-// let letters = currentRow.children;
+function handleEnterKey(event) {
+  if (event.key === "Enter") {
+    handleGuess();
+  }
+}
 
-
+  // Attach an event listener to the guess input for Enter key press
+let guessInput = document.getElementById("guess-input");
+guessInput.addEventListener("keyup", handleEnterKey);
